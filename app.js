cd60042188ee41d320adfef96eb2187ee2358078
Kotzparade <kotzparade@live.de>
2022-03-18 21:30:50 +0100

    loewen

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // Simple three.js example
 
-import * as THREE from "https://threejs.org/build/three.module.js";
-import { OrbitControls } from "https://threejs.org/examples/jsm/controls/OrbitControls.js";
-import { GLTFLoader } from "https://threejs.org/examples/jsm/loaders/GLTFLoader.js";
+import * as THREE from "https://unpkg.com/three@0.126.1/build/three.module.js";
+
+import { GLTFLoader } from "https://unpkg.com/three@0.126.1/examples/jsm/loaders/GLTFLoader.js";
+import { DRACOLoader } from "https://unpkg.com/three@0.126.1/examples/jsm/loaders/DRACOLoader.js";
+import { OrbitControls } from "https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js";
 
 var mesh, renderer, scene, camera, controls;
 
@@ -14,19 +16,21 @@ function init() {
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setPixelRatio(window.devicePixelRatio);
+
   document.body.appendChild(renderer.domElement);
 
   // scene
   scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xffffff);
 
   // camera
   camera = new THREE.PerspectiveCamera(
-    5,
+    10,
     window.innerWidth / window.innerHeight,
     1,
-    10000
+    1000
   );
-  camera.position.set(20, 20, 20);
+  camera.position.set(0, 0, 10);
 
   // controls
   controls = new OrbitControls(camera, renderer.domElement);
@@ -52,8 +56,15 @@ function init() {
   // 3D Loader
   const loader = new GLTFLoader();
 
+  // Optional: Provide a DRACOLoader instance to decode compressed mesh data
+  const dracoLoader = new DRACOLoader();
+  dracoLoader.setDecoderPath(
+    "https://unpkg.com/three@0.126.1/examples/js/libs/draco/"
+  );
+  loader.setDRACOLoader(dracoLoader);
+
   loader.load(
-    "book.gltf",
+    "loewe_fixed.glb",
     function (gltf) {
       scene.add(gltf.scene);
     },
@@ -67,7 +78,7 @@ function init() {
 function animate() {
   requestAnimationFrame(animate);
 
-  //controls.update();
+  controls.update();
 
   renderer.render(scene, camera);
 }
