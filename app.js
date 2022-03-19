8f314c8eb2cf39237ba36f75b9bdd72a18ccced0
Kotzparade <kotzparade@live.de>
2022-03-19 11:21:35 +0100

    gsap

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ import { OrbitControls } from "https://unpkg.com/three@0.126.1/examples/jsm/cont
 
 var mesh, renderer, scene, camera, controls;
 
+let tl = gsap.timeline();
+
 init();
 animate();
 
@@ -25,12 +27,12 @@ function init() {
 
   // camera
   camera = new THREE.PerspectiveCamera(
-    10,
+    1,
     window.innerWidth / window.innerHeight,
     1,
     1000
   );
-  camera.position.set(0, 0, 10);
+  camera.position.set(0, 0, 200);
 
   // controls
   controls = new OrbitControls(camera, renderer.domElement);
@@ -40,11 +42,11 @@ function init() {
 
   // light
   var light = new THREE.DirectionalLight(0xffffff, 1);
-  light.position.set(20, 20, 0);
+  light.position.set(10, 100, 10);
   scene.add(light);
 
   // axes
-  //scene.add(new THREE.AxesHelper(20));
+  // scene.add(new THREE.AxesHelper(20));
 
   // geometry
   //var geometry = new THREE.SphereGeometry(14, 24, 24);
@@ -66,7 +68,48 @@ function init() {
   loader.load(
     "loewe_fixed.glb",
     function (gltf) {
+      gltf.scene.rotation.set(0, 0, 0);
       scene.add(gltf.scene);
+      tl.to(gltf.scene.rotation, { y: 6, duration: 2 });
+      tl.to(gltf.scene.scale, { x: 1, y: 1, z: 1, duration: 1 }, "-=2");
+      tl.to(gltf.scene.position, { x: 0, y: -1, z: 0, duration: 2 }, "-=2");
+
+      /**
+       * Animation GSAP
+       */
+
+      gsap.registerPlugin(ScrollTrigger);
+      ScrollTrigger.defaults({
+        scrub: 1,
+        ease: "none",
+      });
+      const sections = document.querySelectorAll(".section");
+
+      gsap.from("h1", {
+        yPercent: 100,
+        autoAlpha: 0,
+        ease: "back",
+        delay: 0.3,
+      });
+      gsap.to(gltf.scene.rotation, {
+        x: Math.PI * 2,
+        scrollTrigger: {
+          trigger: sections[1],
+        },
+      });
+      gsap.to(gltf.scene.scale, {
+        x: 2,
+        y: 2,
+        scrollTrigger: {
+          trigger: sections[2],
+        },
+      });
+      gsap.to(gltf.scene.rotation, {
+        y: Math.PI * 2,
+        scrollTrigger: {
+          trigger: sections[3],
+        },
+      });
     },
     undefined,
     function (error) {
@@ -75,6 +118,7 @@ function init() {
   );
 }
 
+//Animate Loop
 function animate() {
   requestAnimationFrame(animate);
 
